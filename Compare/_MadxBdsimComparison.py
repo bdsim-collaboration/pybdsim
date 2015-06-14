8475d691d7df982deaf6b1f02d548b5c0a59b5fd
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2015-06-14 18:47:09 +0900

    comparison update for now

--- Compare/_MadxBdsimComparison.py
+++ Compare/_MadxBdsimComparison.py
@@ -1,3 +1,22 @@
+import pymadx as _pymadx
+import pybdsim as _pybdsim
 
-def Madx2Bdsim() : 
-    print 'Madx compared to Gmad files - TBC'
+def CompareMadxAndBDSIM(tfsfile,bdssurveyfile):
+    if type(tfsfile) == str:
+        print 'Loading file using pymadx'
+        madx   = _pymadx.Tfs(input)
+    else:
+        print 'Already a pymadx instance - proceeding'
+        madx   = input
+
+    if type(bdssurveyfile) == str:
+        print 'Loading file using pybdsim'
+        bdsf   = _pybdsim.Data.Load(bdssurveyfile)
+    else:
+        print 'Already a pymadx instance - proceeding'
+        bdsf   = bdssurveyfile
+
+
+    bx = bdsf.X()
+    bz = bdsf.Z()
+
