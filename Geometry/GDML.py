3009b6e971d36b2f463e3d858fffd5eece60d13f
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2015-06-24 16:53:04 +0100

    Start of GDML/CSG/STL system

---
+++ Geometry/GDML.py
@@ -0,0 +1,13 @@
+class GDML : 
+
+    def __init__(self, fileName) : 
+        self.ReadFile(fileName)
+
+    def ReadFile(self,fileName) : 
+        pass
+
+    def ListBodies(self) : 
+        pass 
+
+    def ListMaterials(self) : 
+        pass
