004fa89d9c19251e9e69d580bcfe54a77cff0c31
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2015-09-07 15:23:50 +0100

    primaries->Primaries, added printout for clarity

--- Testing/bdsimPrimaries2Ptc.py
+++ Testing/bdsimPrimaries2Ptc.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as _plt
 
 
 
-def bdsimPrimaries2Ptc(input,outfile):
+def BdsimPrimaries2Ptc(input,outfile):
     """"
     Takes .root file generated from a BDSIM run an an input and creates
     a PTC inrays file from the primary particle tree. Outfile should be .madx
     """    
-    
+
+    print "BdsimPrimaries2Ptc processing..."
     rootin      = _rbs.RobdsimOutput(input)
-    primchain   = rootin.GetSamplerChain('primaries')
+    primchain   = rootin.GetSamplerChain('Primaries')
     arrchain    = _rnp.tree2rec(primchain)   #array form of the primary tree chain
 
     nparticles  = len(arrchain)
