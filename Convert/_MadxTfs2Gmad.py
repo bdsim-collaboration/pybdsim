bd83bc4d85f232cef508d97d06577a3f518a61d5
Andrey Abramov <Andrey.Abramov.2012@live.rhul.ac.uk>
2015-10-28 13:55:47 +0000

    Sets sigmaE for bdsim beam from tfs header

--- Convert/_MadxTfs2Gmad.py
+++ Convert/_MadxTfs2Gmad.py
@@ -387,5 +387,6 @@ def MadxTfs2GmadBeam(tfs, startname=None, verbose=False):
     beam.SetAlphaY(data['ALFY'])
     beam.SetEmittanceX(ex,'m') 
     beam.SetEmittanceY(ey,'m')
+    beam.SetSigmaE(sigmae)
     
     return beam
