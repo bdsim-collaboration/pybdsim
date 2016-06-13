15417be42e01871851733f7b7bcdbfa78ac0a508
William Shields <williamshields@Williams-MacBook-Pro-3.local>
2016-06-13 14:17:03 +0100

    Changed tree2rec to tree2array (tree2rec is depreciated and will be removed from root_numpy in the future).

--- Testing/bdsimMadx.py
+++ Testing/bdsimMadx.py
@@ -172,7 +172,7 @@ class LatticeTest:
             print "robdsim.RobdsimOutput> root file loaded"
         
             primchain = rootdata.GetSamplerChain('Primaries')
-            bdsimprim = _rnp.tree2rec(primchain)
+            bdsimprim = _rnp.tree2array(primchain)
             Bx0 = bdsimprim['x']
             By0 = bdsimprim['y']
             Bxp0 = bdsimprim['xp']
@@ -186,7 +186,7 @@ class LatticeTest:
                     break
             print( "End sampler found to be " + final_sampler)
             endchain = rootdata.GetSamplerChain(final_sampler)
-            bdsim = _rnp.tree2rec(endchain)
+            bdsim = _rnp.tree2array(endchain)
             Bx = bdsim['x']
             By = bdsim['y']
             Bxp = bdsim['xp']
