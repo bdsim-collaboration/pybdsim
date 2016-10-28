6f4c6c0690534141cfae39fde4d9266025087cad
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2016-10-28 12:28:16 +0200

    Favour explicit imports for conversion so we can see what's coming from where when debugging.

--- Convert/__init__.py
+++ Convert/__init__.py
@@ -9,7 +9,16 @@ from .. import Builder as _Builder
 
 from _MadxTfs2Gmad import MadxTfs2Gmad
 from _MadxTfs2GmadStrength import MadxTfs2GmadStrength
-from _Mad8Twiss2Gmad import *
-from _Mad8Saveline2Gmad import *
-from _BdsimPrimaries2Inrays import *
+#from _Mad8Twiss2Gmad import *
+#from _Mad8Saveline2Gmad import *
 
+from _Mad8Saveline2Gmad import Mad8Saveline2Gmad
+
+from _Mad8Twiss2Gmad import Mad8Twiss2Gmad
+from _Mad8Twiss2Gmad import Mad8MakeOptions
+from _Mad8Twiss2Gmad import Mad8MakeApertureTemplate
+from _Mad8Twiss2Gmad import Mad8MakeCollimatorTemplate
+
+from _BdsimPrimaries2Inrays import BdsimPrimaries2Ptc
+from _BdsimPrimaries2Inrays import BdsimPrimaries2Madx
+from _BdsimPrimaries2Inrays import BdsimPrimaries2Mad8
