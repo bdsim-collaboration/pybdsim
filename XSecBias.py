c7c73756152faa30873291969babdb7b856bfbb2
swalker92 <zwap082@live.rhul.ac.uk>
2016-12-03 13:36:03 +0000

    added docstrings and homogenised function parameters
    Now takes string for every argument.  Maybe this is ultimately bad but
    for the mean time makes most sense to meas it sort of reflects the GMAD
    syntax.  Also added doc strings for method, removed a now non-used
    method and removed checks on processes.  There are so many processes and
    so decentralised in definition that better just to trust user.

--- XSecBias.py
+++ XSecBias.py
@@ -1,91 +1,85 @@
-from numpy import arange as _arange
 from re import split as _split
 
-_allowedprocesses = [
-    'eBrem',
-    'eIoni',
-    'msc',
-    'all',
-    'protonInelastic'
-]
-
-_allowedparticles = [
+_allowedparticles = frozenset([
     'e-',
     'e+',
     'proton',
     'gamma'
-]
+])
 
-_allowedflags = [
+_allowedflags = frozenset([
     '1',
     '2',
     '3'
-]
+])
 
 
 class XSecBias(object):
-    """ 
+    """
     A class for containing all information regarding cross section definitions.
     """
     def __init__(self, name, particle, processes, xsecfactors, flags):
         self.name        = self.SetName(name)
         self.particle    = self.SetParticle(particle)
-        self.processlist = self.SetProcessList(processes)
-        self.flaglist    = self.SetFlagList(flags)
-        self.xseclist    = self.SetXSecFactorList(xsecfactors)
+        self.processlist = self.SetProcesses(processes)
+        self.flaglist    = self.SetFlags(flags)
+        self.xseclist    = self.SetXSecFactors(xsecfactors)
         self.CheckBiasedProcesses()
-    
+
     def SetName(self, name):
+        '''
+        Set the bias name.
+        Cannot be any upper/lowercase variant of reserved keyword "xsecBias".
+        '''
         if name.lower() == 'xsecbias':
             raise ValueError("Forbidden name: "+ str(name) +".  Bias name cannot be any variant of 'xsecbias' (case insensitive)")
         return name
-    
+
     def SetParticle(self, particle):
+        """
+        Set the particle for bias to be associated with.
+        """
         if particle  in _allowedparticles:
             return particle
         else:
             raise ValueError("Unknown Particle type: " + str(particle) + ".")
-    
-    def SetProcessList(self, processes):
+
+    def SetProcesses(self, processes):
+        '''
+        Set the list of processes to be biased.
+        processes hould be a space-delimited string of processes.
+        '''
         processlist = _split(', |,| ', processes)
-
-        # as we don't have a complete list just now, prevent this checking.
-        return processlist
-
         if ('all' in processlist) and (len(processlist) > 1):
-            raise Warning("You cannot have both all and another process to be biased.")
-        for process in processlist:
-            if process not in _allowedprocesses:
-                raise ValueError("Unknown process type: " + str(process))
+            raise ValueError("You cannot have both 'all' and another process be biased.")
         return processlist
 
-    
-    def SetFlagList(self, flag):
-        flag = self._MapToString(flag)
-        flaglist = _split(', |,| ',flag)
+    def SetFlags(self, flags):
+        """
+        Set flags.  flags should be a space-delimited string of integers, 1-3,
+        in the same order as the processes,
+        """
+        flaglist = _split(', |,| ',flags)
         for number in flaglist:
             if number not in _allowedflags:
                 raise ValueError("Unknown Flag type: " + str(number))
         return flaglist
-    
-    def SetXSecFactorList(self, xsec):
-        xsec = self._MapToString(xsec)
-        xseclist = _split(', |,| ', xsec)
-        for factor in xsec:
+
+    def SetXSecFactors(self, xsecs):
+        """
+        Set cross section factors.  xsecs should be a space-delimited string
+        of floats, e.g. "1.0 1e13 1234.9"
+        """
+        xseclist = _split(', |,| ', xsecs)
+        for factor in xseclist:
             if factor < 0:
                 raise ValueError("Negative cross section factor: " +str(factor))
         return xseclist
-    
+
     def CheckBiasedProcesses(self):
         if (len(self.xseclist) != len(self.flaglist)) or (len(self.xseclist) != len(self.processlist)):
             raise Warning("There must be a uniquely defined flag and xsecfactor to go  with every listed process.")
 
-    def _MapToString(self, argument):
-        if (type(argument) == list) or (type(argument) == tuple):
-            return map(str, argument)
-        else:
-            return str(argument)
-        
     def __repr__(self):
         particle = 'particle="' + self.particle + '"'
         proc     = 'proc="' + ' '.join(map(str, self.processlist)) + '"'
@@ -93,7 +87,5 @@ class XSecBias(object):
         flag     = 'flag={' + ','.join(map(str,self.flaglist)) + '}'
 
         s = self.name + ": xsecBias, " + ', '.join([particle,proc,xsec,flag]) + ';\n'
-        
+
         return s
-
-
