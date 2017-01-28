b04c14341a95f6e53acf382e49fe0ff0524e81cb
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2017-01-28 21:58:49 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='pybdsim',
+   version='1.0',
+   description='BDSIM python helper package',
+   author='JAI@RHUL',
+   author_email='stewart.boogert@rhul.ac.uk',
+   py_modules = ['__init__','Beam','Builder','Constants','Data','Gmad','Joinhistograms',
+                 'ModelProcessing','Options','Plot','Run','Visualisation','XSecBias','_General'],
+   packages=['Compare','Field','Geometry','Testing'],
+)
