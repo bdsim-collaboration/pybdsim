2dad84b51c0a43a165ca7ac3a0557c51d49995c1
Stewart Boogert <stewart.boogert@rhul.ac.uk>
2017-06-23 14:38:22 +0900

    Basic SAD comparision plots

--- Compare/__init__.py
+++ Compare/__init__.py
@@ -1,3 +1,8 @@
 from _MadxBdsimComparison import *
 from _Mad8BdsimComparison import Mad8Bdsim
 from _TransportBdsimComparison import TransportVsBDSIM
+try :
+    import pysad as _pysad
+    from _SadComparison import SadComparison
+except ImportError :
+    pass
