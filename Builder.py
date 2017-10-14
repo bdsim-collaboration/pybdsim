65e7ee45b04120fedc2d2a9f1bf7ec5f252d38fb
swalker92 <zwap082@live.rhul.ac.uk>
2017-10-14 15:56:23 +0100

    units explicitly written out by ApertureModel in Builder.py
    Just to ensure converted models are easier to interpret.

--- Builder.py
+++ Builder.py
@@ -327,13 +327,16 @@ class ApertureModel(dict):
         self['aper4'] = aper4
 
     def __repr__(self):
-        s =  'apertureType="' + self['apertureType'] + '"'
-        s += ', aper1=' + str(self['aper1'])
-        for i in (2,3,4):
-            value = self['aper'+str(i)]
-            if value > 0:
-                s += ', aper' + str(i) + '=' + str(value)
-        return s
+        # aper1 is always present at least.
+        out = ('apertureType="{}*m", aper1={}*m').format(self["apertureType"],
+                                                         self["aper1"])
+        # Append any non-zero apertures.
+        for i in [2,3,4]:
+            aperKey = "aper{}".format(i)
+            aperValue = self[aperKey]
+            if aperValue > 0:
+                out += ", {}={}*m".format(aperKey, aperValue)
+        return out
 
 class Sampler:
     """
