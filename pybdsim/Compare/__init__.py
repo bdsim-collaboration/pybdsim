b2c2caee949db023f5cba0058d3f24836a0e024c
swalker92 <zwap082@live.rhul.ac.uk>
2017-10-25 00:13:36 +0100

    santitised __init__ files

--- pybdsim/Compare/__init__.py
+++ pybdsim/Compare/__init__.py
@@ -1,6 +1,15 @@
 from _MadxBdsimComparison import *
-from _Mad8BdsimComparison import Mad8Bdsim
 from _TransportBdsimComparison import TransportVsBDSIM
+
+try:
+    import pymad8 as _pymad8
+    from _Mad8BdsimComparison import Mad8Bdsim
+except ImportError:
+    import warnings
+    msg = "Missing pymad8 dependency.  MAD8 comparison facilities excluded."
+    warnings.warn(msg)
+    del warnings
+
 from _BdsimBdsimComparison import BDSIMVsBDSIM
 
 try :
--- pybdsim/Convert/__init__.py
+++ pybdsim/Convert/__init__.py
@@ -3,26 +3,25 @@ Module for various conversions.
 
 """
 
-import numpy as _np
-import pymadx as _pymadx
-from .. import Builder as _Builder
-
 from _MadxTfs2Gmad import MadxTfs2Gmad
 from _MadxTfs2GmadStrength import MadxTfs2GmadStrength
-#from _Mad8Twiss2Gmad import *
-#from _Mad8Saveline2Gmad import *
 
-from _Mad8Saveline2Gmad import Mad8Saveline2Gmad
+try:
+    from _Mad8Saveline2Gmad import Mad8Saveline2Gmad
+    from _Mad8Twiss2Gmad import Mad8Twiss2Gmad
+    from _Mad8Twiss2Gmad import Mad8MakeOptions
+    from _Mad8Twiss2Gmad import Mad8MakeApertureTemplate
+    from _Mad8Twiss2Gmad import Mad8MakeCollimatorTemplate
+except ImportError:
+    import warnings
+    msg = "Missing pymad8 dependency.  MAD8 conversion facilities excluded."
+    warnings.warn(msg)
+    del warnings
 
-from _Mad8Twiss2Gmad import Mad8Twiss2Gmad
-from _Mad8Twiss2Gmad import Mad8MakeOptions
-from _Mad8Twiss2Gmad import Mad8MakeApertureTemplate
-from _Mad8Twiss2Gmad import Mad8MakeCollimatorTemplate
-
-try : 
-    import pysad 
+try:
+    import pysad
     from _SadFlat2Gmad import SadFlat2GMad
-except ImportError :
+except ImportError:
     pass
 
 from _BdsimPrimaries2Inrays import BdsimPrimaries2Ptc
