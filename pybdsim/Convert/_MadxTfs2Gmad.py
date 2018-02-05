bdsim-collaboration/pybdsim

commit 8df677d1da4569e8eb4663b82241df2b3e3d7d6b

Conversion of K1 for dipoles.

pybdsim/Convert/_MadxTfs2Gmad.py
@@ -349,6 +349,9 @@ def MadxTfs2Gmad(tfs, outputfilename, startname=None, stopname=None, stepsize=1,
                 kws['fintx'] = fintx
             if (hgap != 0):
                 kws['hgap'] = hgap
+            if k1l != 0:
+                k1 = k1l / l
+                kws['k1'] = k1
             a.AddDipole(rname,'rbend',l,angle=angle,**kws)
         elif t == 'RCOLLIMATOR' or t == 'ECOLLIMATOR':
             #only use xsize as only have half gap
@@ -405,7 +408,7 @@ def MadxTfs2Gmad(tfs, outputfilename, startname=None, stopname=None, stepsize=1,
             if (e2 != 0):
                 kws['e2'] = e2
             if k1l != 0:
-                k1 = k1l / l * factor
+                k1 = k1l / l
                 kws['k1'] = k1
             if (fint != 0):
                 kws['fint']  = fint

commit 421f53e8bb56ff046aba26b4f415b027f60dcb48

Fixed name error introduced in 8df677d1da4569e8eb4663b82241df2b3e3d7d6b

pybdsim/Convert/_MadxTfs2Gmad.py
@@ -346,6 +346,7 @@ def MadxTfs2Gmad(tfs, outputfilename, startname=None, stopname=None, stepsize=1,
             fint  = item['FINT']
             fintx = item['FINTX']
             hgap = item.get('HGAP', 0.0)
+            k1l = item["K1L"]
 
             if (e1 != 0):
                 kws['e1'] = e1