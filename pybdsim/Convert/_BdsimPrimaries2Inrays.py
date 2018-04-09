07adc20b59c90ae9086f4f0d35414025cdc3cf3e
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2018-04-09 11:41:49 +0200

    Fix testing because of change of variable name.

--- pybdsim/Convert/_BdsimPrimaries2Inrays.py
+++ pybdsim/Convert/_BdsimPrimaries2Inrays.py
@@ -137,7 +137,7 @@ def _LoadBdsimPrimaries(inputfile, start, ninrays):
     xp          =  _rnp.tree2array(tree, branches="Primary.xp")
     y           =  _rnp.tree2array(tree, branches="Primary.y")
     yp          =  _rnp.tree2array(tree, branches="Primary.yp")
-    tof         =  _rnp.tree2array(tree, branches="Primary.t")
+    tof         =  _rnp.tree2array(tree, branches="Primary.T")
     E           =  _rnp.tree2array(tree, branches="Primary.energy")
 
     #Get particle pdg number
