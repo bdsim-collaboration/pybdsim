43463647c8437de1db292f543a8f8de29cbc2434
Joshua Albrecht <joshua.albrecht.2011@live.rhul.ac.uk>
2018-11-26 16:00:47 +0000

    Updated formatting, added proper error message to error case.

--- pybdsim/Convert/_BdsimElement2TransferMatrix.py
+++ pybdsim/Convert/_BdsimElement2TransferMatrix.py
@@ -1,16 +1,18 @@
 import numpy as _np
-import pymad8
 import pybdsim.Data
 
 def BdsimElement2TransferMatrix(bdsfile,element,outputType="list"):
     
     """
-    Calculates the Transfer Matrix of an element in a BDSIM beamline from an output (non-optical) rootfile of the beamline by using a Singular Value Decomposition pseudoinverse of a matrix of particle trajectories.
+    Calculates the Transfer Matrix of an element in a BDSIM beamline from an output (non-optical) 
+    rootfile of the beamline by using a Singular Value Decomposition pseudoinverse of a matrix of 
+    particle trajectories.
     
     Inputs:
     bdsfile - File to use to find the element.
     element - Number of element (must be >0, as requires an element before the element of interest).
-    outputtype - "list", "dict" (default "list"). Controls output type, list returns a 16-element list of floats of the matrix elements, dict returns a dict of labelled elements r11 through r44.
+    outputtype - "list", "dict" (default "list"). Controls output type, list returns a 16-element list of 
+    floats of the matrix elements, dict returns a dict of labelled elements r11 through r44.
     """
 
     rootfile    = pybdsim.Data.Load(bdsfile)
@@ -51,9 +53,12 @@ def BdsimElement2TransferMatrix(bdsfile,element,outputType="list"):
                  }
     
     elif outputType == "list" :
-        output = [xrow.item(0),xrow.item(1),xrow.item(2),xrow.item(3),xprow.item(0),xprow.item(1),xprow.item(2),xprow.item(3),yrow.item(0),yrow.item(1),yrow.item(2),yrow.item(3),yprow.item(0),yprow.item(1),yprow.item(2),yprow.item(3)]
+        output = [xrow.item(0),xrow.item(1),xrow.item(2),xrow.item(3),
+                  xprow.item(0),xprow.item(1),xprow.item(2),xprow.item(3),
+                  yrow.item(0),yrow.item(1),yrow.item(2),yrow.item(3),
+                  yprow.item(0),yprow.item(1),yprow.item(2),yprow.item(3)]
            
     else:
-        print 'invalid output type, use "dict" or "list"'
+        raise TypeError('outputType: "'+outputType+'" invalid.')
         
     return output
