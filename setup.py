11ca68b098bfc6800d48e175c3bdb37c07706655
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2020-04-19 12:57:20 +0200

    Fix compability for Python 2 and 3

--- setup.py
+++ setup.py
@@ -14,8 +14,7 @@ setup(
                       "pytransport"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
-    # Some version of python2.7
-    python_requires="==2.7.*",
+    python_requires=">=2.7.*",
 
     author='JAI@RHUL',
     author_email='laurie.nevay@rhul.ac.uk',
