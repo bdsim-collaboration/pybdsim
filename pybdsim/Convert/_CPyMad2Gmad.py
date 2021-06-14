cee4a0d3ac79e3013c8f1f3ec1ce9f603f1439a8
Cédric Hernalsteens <cedric.hernalsteens@ulb.be>
2021-06-14 11:48:20 +0200

    Update converter

--- pybdsim/Convert/_CPyMad2Gmad.py
+++ pybdsim/Convert/_CPyMad2Gmad.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import Optional, Dict
 import os
 import re
+import logging
 
 import yaml
 import pandas as _pd
@@ -26,7 +27,7 @@ BDSIM_RESERVED_NAME = {
 }
 
 BDSIM_ELEMENTS_ARGUMENTS = ['l',
-                            'k1',cd
+                            'k1',
                             'k2',
                             'angle',
                             'knl',
@@ -35,10 +36,11 @@ BDSIM_ELEMENTS_ARGUMENTS = ['l',
                             'aperture']
 
 # Keep these elements (in BDSIM convention)
-ELEMENTS_TO_KEEP = ['drift', 'sbend', 'rbend', 'quadrupole', 'sextupole', 'octupole', 'hkicker', 'vkicker', 'solenoid',
+BDSIM_ELEMENTS_TO_GENERATE = ['drift', 'sbend', 'rbend', 'quadrupole', 'sextupole', 'octupole', 'hkicker', 'vkicker', 'solenoid',
                     'rcol', 'multipole', 'thinmultipole']
 
-ELEMENTS_TO_DROP = ['monitor', 'marker', 'instrument']
+"""Elements that are dropped immediately without any processing when reading the MAD-X sequence."""
+MAD_ELEMENTS_TO_DROP = ['marker', 'monitor']
 
 
 class CPyMad2Gmad:
@@ -63,17 +65,20 @@ class CPyMad2Gmad:
                     'data.yml')
             ) as file:
                 self.model = merge(self.model, yaml.full_load(file))
+                logging.debug(f"Merged data from module {module}.")
         self.model = merge(self.model, _)
         self.model = merge(self.model, model or {})
 
         # Add quotes for BDSim options
         for k, v in self.model['options'].items():
-            if isinstance(v, str):
+            if isinstance(v, str) and '*' not in v:
                 self.model['options'][k] = '"' + v + '"'
 
         # Compile the regular expressions in the model
+        logging.debug("Compiling regular expressions...")
         for r in list(self.model['sequence'].keys()):
             self.model['sequence'][re.compile(r)] = self.model['sequence'].pop(r)
+        logging.debug("All regular expressions compiled.")
 
         # MAD-X
         self.madx = madx_instance
@@ -87,9 +92,12 @@ class CPyMad2Gmad:
                 parent = 'thinmultipole'
                 base_parent = 'thinmultipole'
                 length = None
-            elif base_parent != 'multipole' and length == 0:
-                parent = 'marker'
-                base_parent = 'marker'
+                logging.warning(f"{element.name} (of type {element.parent.name} / {element.base_type.name})"
+                                f" - Converting to thinmultipole because it is a zero-length multipole.")
+            elif base_parent != 'multipole' and length == 0 and level == 0:
+                logging.warning(f"{element.name} (of type {element.parent.name} / {element.base_type.name})"
+                                f" - Dropping element because its length is zero.")
+                base_parent = None
 
             return {
                 'NAME': element.name,
@@ -106,10 +114,10 @@ class CPyMad2Gmad:
         def build_component_recursive(element, i, level=0):
             nonlocal bottom
             if element.parent.name != element.name and element.base_type.name != 'marker':
-                build_component_recursive(element.parent, i, level - 1)
                 component = build_component(element, i, level)
-                if component['BASE_PARENT'] in ELEMENTS_TO_KEEP:
+                if component['BASE_PARENT'] is not None:
                     components.append(component)
+                    build_component_recursive(element.parent, i, level - 1)
             else:
                 bottom = level
 
@@ -131,8 +139,13 @@ class CPyMad2Gmad:
             idx1 = expanded_elements.index(self.model['builder']['from'] or expanded_elements[0].name)
             idx2 = expanded_elements.index(self.model['builder']['to'] or expanded_elements[-1].name)
             for i in range(idx1, idx2+1):
-                if expanded_elements[i].base_type.name not in ELEMENTS_TO_DROP:
-                    yield expanded_elements[i]
+                element = expanded_elements[i]
+                if expanded_elements[i].base_type.name not in MAD_ELEMENTS_TO_DROP:
+                    yield element
+                else:
+                    assert element['l'] == 0.0
+                    logging.info(f"{element.name} (of type {element.parent.name} / {element.base_type.name})"
+                                 f" - Skipping element because it should be dropped (length = {element['l']}).")
         return _iterator
 
     def _get_model_properties_for_element(self, element_name):
@@ -147,9 +160,12 @@ class CPyMad2Gmad:
         daughter_keys = element.defs
         non_default_args = {
             k: daughter_keys[k] for k in parent_keys if
-                            k in daughter_keys and parent_keys[k] != daughter_keys[k]
+                            k in BDSIM_ELEMENTS_ARGUMENTS and k in daughter_keys and parent_keys[k] != daughter_keys[k] and k != 'at'
         }
-
+        if not bool(non_default_args):  # Bare aliases not allowed in BDSim
+            non_default_args = {'l': element.defs['l']}
+            logging.info(f"{element.name} (of type {element.parent.name} / {element.base_type.name})"
+                            f" - Adding the length in the definition because bare aliases are not allowed.")
         bdsim_properties = {}
         for k, i in non_default_args.items():
             if k in BDSIM_ELEMENTS_ARGUMENTS:
@@ -168,10 +184,9 @@ class CPyMad2Gmad:
                 else:
                     bdsim_properties[BDSIM_MAD_CONVENTION.get(k, k)] = element[k]
 
-        return self.__class__._adjust_bdsim_component_properties(bdsim_properties)
+        return self._adjust_bdsim_component_properties(element, bdsim_properties)
 
-    @staticmethod
-    def _adjust_bdsim_component_properties(bdsim_properties):
+    def _adjust_bdsim_component_properties(self, element, bdsim_properties):
         if 'apertureType' in bdsim_properties:
             bdsim_properties['apertureType'] = BDSIM_MAD_CONVENTION.get(bdsim_properties['apertureType'], bdsim_properties['apertureType'])
         if bdsim_properties.get('apertureType') == 'octagonal':
@@ -180,6 +195,13 @@ class CPyMad2Gmad:
             aper4_mad = bdsim_properties['aper4']
             bdsim_properties['aper3'] = bdsim_properties['aper2'] / _np.tan(aper4_mad)
             bdsim_properties['aper4'] = bdsim_properties['aper1'] * _np.tan(aper3_mad)
+        for key in ('aper1', 'aper2', 'aper3', 'aper4'):
+            if bdsim_properties.get(key, 0.0) > self.model['builder']['config']['apertures']['remove_above_value']:
+                logging.warning(f"Dropping the key {key} of element {element.name} because it exceeds the treshold value")
+                del bdsim_properties[key]
+        if 'aper1' not in bdsim_properties and 'aper2' not in bdsim_properties and 'aper3' not in bdsim_properties and 'aper4' not in bdsim_properties and 'apertureType' in bdsim_properties:
+            del bdsim_properties['apertureType']
+            logging.warning(f'Dropping the apertureType key of {element.name} because it has no aperture')
 
         return bdsim_properties
 
@@ -192,14 +214,14 @@ class CPyMad2Gmad:
         element_name = BDSIM_RESERVED_NAME.get(name, name)
         parent_name = BDSIM_RESERVED_NAME.get(element['PARENT'], element['PARENT'])
 
-        if element['BASE_PARENT'] not in ELEMENTS_TO_KEEP:
+        if element['BASE_PARENT'] not in BDSIM_ELEMENTS_TO_GENERATE:
+            print(element_name)
+            print(element['PARENT'])
             bdsim_element = pybdsim.Builder.Element(name=element_name,
                                                      category='drift',
                                                      isMultipole=False,
                                                      l=element['L'])
         else:
-            if not bool(bdsim_dict_arg):
-                bdsim_dict_arg = {'l': 0.0}  # Bare aliases not allowed in BDSim
             if element['LEVEL'] == 1:
                 bdsim_element = pybdsim.Builder.Element(name=element_name,
                                                         category=parent_name,
@@ -235,18 +257,25 @@ class CPyMad2Gmad:
             bdsim_input.Append(component['BDSIM'], is_component=False)
 
         if with_beam:
+            if 'twiss' not in self.madx.table:
+                self.madx.input('twiss;')
+            tw = self.madx.table['twiss'].dframe().iloc[0]
             bdsim_beam = pybdsim.Beam.Beam(particletype=self.madx_beam['particle'],
-                                           energy=self.madx_beam['energy'] + self.madx_beam['mass'],
+                                           energy=self.madx_beam['energy'],
                                            distrtype='gausstwiss')
-            bdsim_beam.SetBetaX(self.madx_beam['beta'])
-            bdsim_beam.SetBetaY(self.madx_beam['beta'])
-            bdsim_beam.SetAlphaX(self.madx_beam['alfa'])
-            bdsim_beam.SetAlphaY(self.madx_beam['alfa'])
+            bdsim_beam.SetX0(tw['x'])
+            bdsim_beam.SetXP0(tw['px'])
+            bdsim_beam.SetY0(tw['y'])
+            bdsim_beam.SetYP0(tw['py'])
+            bdsim_beam.SetBetaX(tw['betx'])
+            bdsim_beam.SetBetaY(tw['bety'])
+            bdsim_beam.SetAlphaX(tw['alfx'])
+            bdsim_beam.SetAlphaY(tw['alfy'])
 
             bdsim_beam.SetEmittanceX(self.madx_beam['ex'])
             bdsim_beam.SetEmittanceY(self.madx_beam['ey'])
-            bdsim_beam.SetSigmaE(self.madx_beam['sige'])
-            bdsim_beam.SetSigmaT(self.madx_beam['sigt'])
+            # bdsim_beam.SetSigmaE(self.madx_beam['sige'])
+            # bdsim_beam.SetSigmaT(self.madx_beam['sigt'])
             bdsim_input.AddBeam(bdsim_beam)
 
         if with_options:
