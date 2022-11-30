65abf329a0e3a4ed3db41b24e06e8eba872d7649
Laurie Nevay <laurie.nevay@rhul.ac.uk>
2022-11-30 09:40:13 +0100

    Line wrapping and indentation.

--- pybdsim/Builder.py
+++ pybdsim/Builder.py
@@ -234,8 +234,7 @@ class Element(ElementBase):
                 elif (type(self[key]) == tuple
                       and ((self.category == 'thinmultipole')
                            or (self.category == 'multipole'))):
-                    s += (', ' + key + '=' + '{'
-                          + (','.join([str(s) for s in self[key]]))+'}')
+                    s += (', ' + key + '=' + '{' + (','.join([str(s) for s in self[key]]))+'}')
                 else:
                     s += ", {}={}".format(key, self[key])
         s += ';\n'
@@ -720,13 +719,13 @@ class Transform3D(Element):
 
 class Rmat(Element):
     def __init__(self, name, l, r11, r12, r13, r14,
-				r21, r22, r23, r24,
-				r31, r32, r33, r34, 
-				r41, r42, r43, r44,  **kwargs):
+                 r21, r22, r23, r24,
+                 r31, r32, r33, r34,
+                 r41, r42, r43, r44,  **kwargs):
         Element.__init__(self, name, 'rmatrix', l=l, r11=r11, r12=r12, r13=r13, r14=r14,
-						r21=r21, r22=r22, r23=r23, r24=r24,
-						r31=r31, r32=r32, r33=r33, r34=r34,
-						r41=r41, r42=r42, r43=r43, r44=r44, **kwargs)
+                         r21=r21, r22=r22, r23=r23, r24=r24,
+                         r31=r31, r32=r32, r33=r33, r34=r34,
+                         r41=r41, r42=r42, r43=r43, r44=r44, **kwargs)
 
 
 class Sampler(object):
