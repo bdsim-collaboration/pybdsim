0b2294f89421cbd4997e16399bae763b23ab8725
Laurie Nevay <laurie.nevay@cern.ch>
2023-03-19 14:04:45 +0100

    Fix dynamic version.

--- src/pybdsim/__init__.py
+++ src/pybdsim/__init__.py
@@ -51,6 +51,13 @@ Copyright Royal Holloway, University of London 2023.
 
 """
 
+try:
+    from ._version import version as __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown version"
+    version_tuple = (0, 0, "unknown version")
+
 from . import Beam
 from . import Builder
 from . import Constants
