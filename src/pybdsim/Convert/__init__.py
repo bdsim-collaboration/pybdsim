5f7585678ebcb2bc58df0389764ca6557f76ef68
Laurie Nevay <laurie.nevay@cern.ch>
2023-03-19 13:01:49 +0100

    Silence warnings about optional imports.

--- src/pybdsim/Convert/__init__.py
+++ src/pybdsim/Convert/__init__.py
@@ -7,45 +7,21 @@ from ._MadxTfs2Gmad import MadxTfs2Gmad
 from ._MadxTfs2Gmad import ZeroMissingRequiredColumns as _ZeroMissingRequiredColumns
 from ._MadxTfs2GmadStrength import MadxTfs2GmadStrength
 from ._MadxTfs2Gmad import MadxTfs2GmadBeam
+from ._Mad8Twiss2Gmad import Mad8Twiss2Gmad
+from ._Transport2Gmad import Transport2Gmad
 
-try:
-    # from ._Mad8Saveline2Gmad import Mad8Saveline2Gmad
-    # from ._Mad8Twiss2Gmad import Mad8Twiss2Gmad
-    # from ._Mad8Twiss2Gmad import Mad8MakeOptions
-    # from ._Mad8Twiss2Gmad import Mad8MakeApertureTemplate
-    # from ._Mad8Twiss2Gmad import Mad8MakeCollimatorTemplate
-    from ._Mad8Twiss2Gmad import Mad8Twiss2Gmad
-except ImportError:
-    import warnings
-    msg = "Missing pymad8 dependency.  MAD8 conversion facilities excluded."
-    warnings.warn(msg)
-    del warnings
+# all optional imports are quiet without warning so we don't see print out all the time for each one
 
 try:
     import pysad
     from ._SadFlat2Gmad import SadFlat2GMad
 except ImportError:
     pass
-    #import warnings
-    #msg = "Missing pysad dependency. SAD conversion facilities excluded."
-    #warnings.warn(msg)
-    #del warnings
-
-try:
-    from ._Transport2Gmad import Transport2Gmad
-except ImportError:
-    import warnings
-    msg = "Missing pytransport dependency.  TRANSPORT conversion facilities excluded."
-    warnings.warn(msg)
-    del warnings
 
 try:
     from ._CPyMad2Gmad import CPyMad2Gmad
 except ImportError:
-    import warnings
-    msg = "Missing cpymad dependency. cpymad conversion facilities excluded."
-    warnings.warn(msg)
-    del warnings
+    pass
 
 from ._BdsimPrimaries2Inrays import BdsimPrimaries2Ptc
 from ._BdsimPrimaries2Inrays import BdsimPrimaries2Madx
