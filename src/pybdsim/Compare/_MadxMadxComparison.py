6efdd560199f6427f2cadf52487b792962e94461
Laurie Nevay <laurie.nevay@cern.ch>
2023-08-20 20:08:36 +0200

    Update for change to pymadx.

--- src/pybdsim/Compare/_MadxMadxComparison.py
+++ src/pybdsim/Compare/_MadxMadxComparison.py
@@ -6,4 +6,4 @@ def MadxVsMadx(first, second, first_name=None,
     Display all the optical function plots for the two input optics files.
     """
     import pymadx as _pymadx
-    _pymadx.Compare.MADXVsMADX(first, second, first_name, second_name, **kwargs)
+    _pymadx.Compare.MadxVsMadx(first, second, first_name, second_name, **kwargs)
