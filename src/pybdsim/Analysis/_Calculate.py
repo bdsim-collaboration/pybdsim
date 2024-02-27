12769a5668f6514ecb774d14da5ddb4f1f09ced8
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2024-02-27 22:33:55 +0000

    Analysis : 6d matrix extraction. Matrix comparison

--- src/pybdsim/Analysis/_Calculate.py
+++ src/pybdsim/Analysis/_Calculate.py
@@ -1,7 +1,8 @@
 import pybdsim.Data as _Data
 import numpy as _np
+import matplotlib.pyplot as _plt
 
-def CalculateRMatrix(root_file_name, sampler1_name, sampler2_name, size=4) :
+def CalculateRMatrix(root_file_name, sampler1_name, sampler2_name, size=4, average = False) :
     """
     **CalculateRMatrix** calculate rmatrix from a BDSIM output root file and two sampler names
 
@@ -21,6 +22,9 @@ def CalculateRMatrix(root_file_name, sampler1_name, sampler2_name, size=4) :
     | sampler2_name   | Second sampler name to take coordinates for calculation |
     +-----------------+---------------------------------------------------------+
     | size (int)      | 4 (x, x', y, y') or 6 (x,x',y,y',T,E)                   |
+    +-----------------+---------------------------------------------------------+
+    | average (bool)  | True : Compute average T and E for subtraction          |
+    |                 | False : Use first particle for  T and E for subtraction |
     +-----------------+---------------------------------------------------------+
     """
 
@@ -44,19 +48,30 @@ def CalculateRMatrix(root_file_name, sampler1_name, sampler2_name, size=4) :
         sampler1_energy = sampler1_data.data['energy']
         sampler2_energy = sampler2_data.data['energy']
 
+        if average :
+            sampler1_time = sampler1_time - sampler1_time.mean()
+            sampler2_time = sampler2_time - sampler2_time.mean()
+            sampler1_energy = sampler1_energy - sampler1_time.mean()
+            sampler2_energy = sampler2_energy - sampler2_time.mean()
+        else :
+            sampler1_time = sampler1_time - sampler1_time[0]
+            sampler2_time = sampler2_time - sampler2_time[0]
+            sampler1_energy = sampler1_energy - sampler1_time[0]
+            sampler2_energy = sampler2_energy - sampler2_time[0]
+
         sampler1_matrix = _np.array([sampler1_data.data['x'],
                                      sampler1_data.data['xp'],
                                      sampler1_data.data['y'],
                                      sampler1_data.data['yp'],
-                                     sampler1_time - sampler1_time.mean(),
-                                     sampler1_energy - sampler1_energy.mean()])
+                                     sampler1_time,
+                                     sampler1_energy])
 
         sampler2_matrix = _np.array([sampler2_data.data['x'],
                                      sampler2_data.data['xp'],
                                      sampler2_data.data['y'],
                                      sampler2_data.data['yp'],
-                                     sampler2_time - sampler2_time.mean(),
-                                     sampler2_energy-sampler2_energy.mean()])
+                                     sampler2_time,
+                                     sampler2_energy])
 
     sampler1_matrix_inv = _np.linalg.pinv(sampler1_matrix)
 
@@ -142,12 +157,42 @@ def CalculateEnergyGain(root_file_name, sampler1_name, sampler2_name) :
 
     return deltaE.mean()
 
-def CompareRMatrix(rmatrix1, rmatrix2) :
+def CompareRMatrix(rmatrix1, rmatrix2, toll = 1e-4, user_print = True) :
+    """
+    **CompareRMatrix** compare two rmatrices. If all elements differences are less than toll
+
+    Example:
+
+    >>> m1 = numpy.array([[1,2],[3,4]])
+    >>> m2 = numpy.array([[1,2],[3,4]])
+    >>> rmatrix = pybdsim.Analysis.CompareRMatrix(m1,m2)
+
+    returns boolean
+
+    +-----------------+---------------------------------------------------------+
+    | **Parameters**  | **Description**                                         |
+    +-----------------+---------------------------------------------------------+
+    | rmatrix1        | First numpy matrix                                      |
+    +-----------------+---------------------------------------------------------+
+    | rmatrix2        | Second numpy matrix                                     |
+    +-----------------+---------------------------------------------------------+
+    | toll            | Tollerence                                              |
+    +-----------------+---------------------------------------------------------+
+    | user_print      | Some print out for user                                 |
+    +-----------------+---------------------------------------------------------+
+    """
+
     # difference matrix
     dmatrix = rmatrix2 - rmatrix1
 
+    bmatrix = abs(dmatrix) > toll
 
+    if bmatrix.any() :
+        print(dmatrix/toll)
+        print(bmatrix*1)
 
-def PlotRMatrix(rmatrix1) :
-    pass
+    return not bmatrix.any()
 
+def PlotRMatrix(rmatrix) :
+    _plt.imshow(rmatrix)
+    _plt.colorbar()
