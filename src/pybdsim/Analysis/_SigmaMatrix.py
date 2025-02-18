e572ab302afd1721297af669904761776d101f26
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-02-18 00:04:51 +0000

    Clearer DataFrame import

--- src/pybdsim/Analysis/_SigmaMatrix.py
+++ src/pybdsim/Analysis/_SigmaMatrix.py
@@ -1,12 +1,12 @@
 from numpy import cov as _cov
 from numpy import ndarray as _ndarray
-from pandas import DataFrame as _df
+from pandas import DataFrame as _DataFrame
 
 def CalculateSigmaMatrix(matrixIn) :
 
     if isinstance(matrixIn, _ndarray):
         na = matrixIn
-    elif isinstance(matrixIn, _df) :
+    elif isinstance(matrixIn, _DataFrame) :
         na = matrixIn.to_numpy().transpose()
     #elif isinstance(matrixIn, dict) :
     #    pass
