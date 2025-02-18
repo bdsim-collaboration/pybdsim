27f1a5f3a033b518aac5b9f289279447a0dfae73
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-02-18 00:55:44 +0000

    Start of likelihood histogram/data comparison function

--- src/pybdsim/Testing/CompareHistogram.py
+++ src/pybdsim/Testing/CompareHistogram.py
@@ -1,6 +1,7 @@
 from numpy import sqrt as _sqrt
 from numpy import isnan as _isnan
 from numpy import bitwise_not as _bitwise_not
+from numpy import log as _log
 
 def compare_hist1d_hist1d(h1, h2) :
     d = h1.contents-h2.contents
@@ -12,6 +13,13 @@ def compare_hist1d_hist1d(h1, h2) :
 def compare_hist1d_array(h1, data) :
     # normalise h1 into pdf like distribution
     h1.contents = h1.contents/h1.contents.sum()
+
+    # compute liklihood
+    lh = 1.0
+    for d in data :
+        p = h1.GetContentsAt(d)
+        lh *= p
+    return _log(lh)
 
 def compare_hist2d_hist2d(h1, h2) :
     d = h1.contents-h2.contents
