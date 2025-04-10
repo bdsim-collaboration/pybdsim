7fd5e671a410512e544780513393ed87a23b901f
marindeniaud <pkap009@live.rhul.ac.uk>
2025-04-10 10:34:56 +0100

    Fix Ptarmigan external module

--- src/pybdsim/External/__init__.py
+++ src/pybdsim/External/__init__.py
@@ -1,5 +1,5 @@
-from Ptarmigan import getBdsimLastSamplerInDF
-from Ptarmigan import writeBdsimDataInH5
+from .Ptarmigan import getBdsimLastSamplerInDF
+from .Ptarmigan import writeBdsimDataInH5
 
-from Ptarmigan import getH5DataInDict
-from Ptarmigan import writeDataInBdsim
+from .Ptarmigan import getH5DataInDict
+from .Ptarmigan import writeDataInBdsim
