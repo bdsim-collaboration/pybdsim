adaf48e15bc05164ac152bb970889ff136c1d135
flstumme <florian.wolfgang.stummer@cern.ch>
2025-04-23 11:13:44 +0200

    fixed some copy paste errors for circle beams

--- src/pybdsim/Beam.py
+++ src/pybdsim/Beam.py
@@ -253,28 +253,28 @@ class Beam(dict) :
         self['shellYp'] = str(shellyp)
 
     def _SetEnvelopeR(self, enveloper=1.0, unitsstring='um'):
-        self['envelopeX'] = str(enveloper) + '*' + unitstring
+        self['envelopeR'] = str(enveloper) + '*' + unitsstring
 
-    def _SetEnvelopeRp(self, enveloperp=1.0):
-        self['envelopeY'] = str(enveloperp) + '*' + unitstring
+    def _SetEnvelopeRp(self, enveloperp=1.0, unitsstring='mrad'):
+        self['envelopeRp'] = str(enveloperp) + '*' + unitsstring
 
-    def _SetEnvelopeT(self, envelopet=1.0,unitstring='s'):
-        self['envelopeX'] = str(envelopet) + '*' + unitstring
+    def _SetEnvelopeT(self, envelopet=1.0,unitsstring='s'):
+        self['envelopeT'] = str(envelopet) + '*' + unitsstring
 
-    def _SetEnvelopeE(self, envelopee=1.0,unitstring='GeV'):
-        self['envelopeY'] = str(envelopee) + '*' + unitstring
+    def _SetEnvelopeE(self, envelopee=1.0,unitsstring='GeV'):
+        self['envelopeE'] = str(envelopee) + '*' + unitsstring
 
-    def _SetEnvelopeX(self, envelopex=1.0,unitstring='m'):
-        self['envelopeX'] = str(envelopex) + '*' + unitstring
+    def _SetEnvelopeX(self, envelopex=1.0,unitsstring='m'):
+        self['envelopeX'] = str(envelopex) + '*' + unitsstring
 
-    def _SetEnvelopeY(self, envelopey=1.0,unitstring='m'):
-        self['envelopeY'] = str(envelopey) + '*' + unitstring
+    def _SetEnvelopeY(self, envelopey=1.0,unitsstring='m'):
+        self['envelopeY'] = str(envelopey) + '*' + unitsstring
 
-    def _SetEnvelopeXp(self, envelopexp=1.0):
-        self['envelopeXp'] = str(envelopexp) + '*' + unitstring
+    def _SetEnvelopeXp(self, envelopexp=1.0,unitsstring='mrad'):
+        self['envelopeXp'] = str(envelopexp) + '*' + unitsstring
 
-    def _SetEnvelopeYp(self, envelopeyp=1.0):
-        self['envelopeYp'] = str(envelopeyp)
+    def _SetEnvelopeYp(self, envelopeyp=1.0,unitsstring='mrad'):
+        self['envelopeYp'] = str(envelopeyp) + '*' + unitsstring
 
     def _SetHaloNSigmaXInner(self, halonsigmaxinner=1):
         self['haloNSigmaXInner'] = str(halonsigmaxinner)
