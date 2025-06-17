372dd38a02fd85ce30d10a24280513c821a82f89
Laurie Nevay <laurie.nevay@cern.ch>
2025-06-17 18:21:40 +0200

    Fix another import. Not a dependency and only used in one function we might not use so often to put in the function.

--- src/pybdsim/Field/_Field.py
+++ src/pybdsim/Field/_Field.py
@@ -1,7 +1,6 @@
 import gzip as _gzip
 import numpy as _np
 import tarfile as _tarfile
-import importlib_resources as _importlib_resources
 import os as _os    
 import pybdsim
 
@@ -684,6 +683,8 @@ def SortUnorderedFieldMap2D(field, symmetry="none", transpose=False):
     :param symmetry: Symmetry to apply to the field
     :type  symmetry: str
     """
+    # this is not a dependency...
+    import importlib_resources as _importlib_resources
     for sym in symmetry.split():
         if sym in _Symmetries2D.keys():
             if sym in ['reflectx', 'reflectxydipole', 'reflectxyquadrupole']:
