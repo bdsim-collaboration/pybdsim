ee19be1549573ef2a9f290c5055a7e38b3cd70a9
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2025-06-21 21:55:50 +0100

    Update RMatrix calc

--- src/pybdsim/Analysis/_RMatrix.py
+++ src/pybdsim/Analysis/_RMatrix.py
@@ -86,7 +86,7 @@ def CalculateRMatrix(root_file_name, sampler1_name, sampler2_name, size=4, avera
 
     return _np.dot(sampler2_matrix, sampler1_matrix_inv)
 
-def CalculateTaylorMapOrder2(root_file_name, sampler1_name, sampler2_name) :
+def CalculateTaylorMapOrder2(root_file_name, sampler1_name, sampler2_name, average=True) :
     """
     **CalculateTaylorMapOrder2** calculate 2nd order Taylor map from a BDSIM output root file and two sampler names
 
@@ -111,24 +111,55 @@ def CalculateTaylorMapOrder2(root_file_name, sampler1_name, sampler2_name) :
     sampler1_data = _Data.SamplerData(root_file, sampler1_name)
     sampler2_data = _Data.SamplerData(root_file, sampler2_name)
 
-    sampler1_matrix = _np.array([sampler1_data.data['x'],
-                                 sampler1_data.data['xp'],
-                                 sampler1_data.data['x'] * sampler1_data.data['x'],
-                                 sampler1_data.data['x'] * sampler1_data.data['xp'],
-                                 sampler1_data.data['x'] * sampler1_data.data['y'],
-                                 sampler1_data.data['x'] * sampler1_data.data['yp'],
-                                 sampler1_data.data['y'],
-                                 sampler1_data.data['yp'],
-                                 sampler1_data.data['y'] * sampler1_data.data['xp'],
-                                 sampler1_data.data['y'] * sampler1_data.data['y'],
-                                 sampler1_data.data['y'] * sampler1_data.data['yp'],
-                                 sampler1_data.data['xp'] * sampler1_data.data['xp'],
-                                 sampler1_data.data['yp'] * sampler1_data.data['yp']])
+    sampler1_x  = sampler1_data.data['x']
+    sampler1_xp = sampler1_data.data['xp']
+    sampler1_y  = sampler1_data.data['y']
+    sampler1_yp = sampler1_data.data['yp']
 
-    sampler2_matrix = _np.array([sampler2_data.data['x'],
-                                 sampler2_data.data['xp'],
-                                 sampler2_data.data['y'],
-                                 sampler2_data.data['yp']])
+    sampler2_x  = sampler2_data.data['x']
+    sampler2_xp = sampler2_data.data['xp']
+    sampler2_y  = sampler2_data.data['y']
+    sampler2_yp = sampler2_data.data['yp']
+
+    sampler1_time = sampler1_data.data['T']
+    sampler2_time = sampler2_data.data['T']
+    sampler1_energy = sampler1_data.data['energy']
+    sampler2_energy = sampler2_data.data['energy']
+
+    if average :
+        sampler1_x  = sampler1_x - sampler1_x.mean()
+        sampler1_xp = sampler1_xp - sampler1_xp.mean()
+        sampler1_y  = sampler1_y - sampler1_y.mean()
+        sampler1_yp = sampler1_yp - sampler1_yp.mean()
+
+        sampler2_x  = sampler2_x - sampler2_x.mean()
+        sampler2_xp = sampler2_xp - sampler2_xp.mean()
+        sampler2_y  = sampler2_y - sampler2_y.mean()
+        sampler2_yp = sampler2_yp - sampler2_yp.mean()
+
+        sampler1_time = sampler1_time - sampler1_time.mean()
+        sampler2_time = sampler2_time - sampler2_time.mean()
+        sampler1_energy = sampler1_energy - sampler1_energy.mean()
+        sampler2_energy = sampler2_energy - sampler2_energy.mean()
+
+    sampler1_matrix = _np.array([sampler1_x,
+                                 sampler1_xp,
+                                 sampler1_y,
+                                 sampler1_yp,
+                                 sampler1_x * sampler1_x,
+                                 sampler1_x * sampler1_xp,
+                                 sampler1_x * sampler1_y,
+                                 sampler1_x * sampler1_yp,
+                                 sampler1_y * sampler1_xp,
+                                 sampler1_y * sampler1_y,
+                                 sampler1_y * sampler1_yp,
+                                 sampler1_xp * sampler1_xp,
+                                 sampler1_yp * sampler1_yp])
+
+    sampler2_matrix = _np.array([sampler2_x,
+                                 sampler2_xp,
+                                 sampler2_y,
+                                 sampler2_yp])
 
     sampler1_matrix_inv = _np.linalg.pinv(sampler1_matrix)
 
