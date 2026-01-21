593c47dedb5f266dc0d6feeb1ed5ed58308af54a
marindeniaud <pkap009@live.rhul.ac.uk>
2026-01-21 16:59:08 +0000

    Add 'try' for ptarmigan script import

--- src/pybdsim/External/__init__.py
+++ src/pybdsim/External/__init__.py
@@ -1,7 +1,10 @@
-from .Ptarmigan import getBdsimLastSamplerInDF
-from .Ptarmigan import writeBdsimDataInH5
+try:
+    from .Ptarmigan import getBdsimLastSamplerInDF
+    from .Ptarmigan import writeBdsimDataInH5
 
-from .Ptarmigan import getH5DataInDict
-from .Ptarmigan import writeDataInBdsim
+    from .Ptarmigan import getH5DataInDict
+    from .Ptarmigan import writeDataInBdsim
+except:
+    pass
 
 from . import Pymad8
