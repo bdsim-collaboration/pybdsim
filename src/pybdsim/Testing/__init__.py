0fec5725f6b2594064a405d6f695c1bf1d8a6c3f
Marie <marie.manniecorbisier@gmail.com>
2026-06-12 15:18:07 +0100

    Functions added for the sbend matrix calculations

--- src/pybdsim/Testing/__init__.py
+++ src/pybdsim/Testing/__init__.py
@@ -6,5 +6,15 @@ from .CompareMatrix import max_matrix_diff
 
 from .CompareOptics import compare_optics_files
 
+from .RotateMatrix import rotated_matrix
+from .RotateMatrix import fringe_T_entrance
+from .RotateMatrix import fringe_T_exit
+from .RotateMatrix import compose_order2
+from .RotateMatrix import edge_matrix
+from .RotateMatrix import tensor_to_pybdsim_tmap
+from .RotateMatrix import roll_matrix_bdsim
+from .RotateMatrix import rotate_order2_map_lab_to_magnet
+from .RotateMatrix import edge_R_matrix, edge_T_matrix
+
 from .CompareHistogram import compare_hist1d_hist1d
 from .CompareHistogram import compare_hist1d_array
